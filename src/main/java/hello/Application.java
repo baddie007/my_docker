c7a6910fda21aad661e9474955b5f06c3a5c3c90
RaghavGeek <52686078+RaghavGeek@users.noreply.github.com>
2020-08-19 12:23:52 +0530

    Update Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
   @RequestMapping("/")
   public String home() {
-    return "Hello Raghav";
+    return "Hello Raghav Arora";
   }
 
   public static void main(String[] args) {
